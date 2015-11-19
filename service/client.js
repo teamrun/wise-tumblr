e007073e73d18d0f3ee4e3f1b498be1fde7d5f3f
teamrun <chenllos@163.com>
2015-11-19 16:51:02 +0800

    client初始化抽出

---
+++ service/client.js
@@ -0,0 +1,36 @@
+import _ from 'lodash';
+import clientCreator from '../util/clientCreator';
+
+let clients = {};
+let names = [];
+const base_conf = {
+  consumer_key: 'LSOJqOOqefr0zc3nFoTyeIwqs7JeeoHRBE0E6A4N573zwX9V2W',
+  consumer_secret: 'ATdwEZ8DlfprOiUGqReAoHJZAjs5lEHOuDZ8khAvhZzEdYujKM',
+};
+
+let confs = [
+  {
+    name: 'libertyartchen',
+    token: 'LWyVXvxt4AAVT0lMT370pwBRFhoxKna8ibAH2SzSwaqxTNJV9U',
+    token_secret: 'LDgi43Yfd53EezKZyhlU74TkeQRtd5xMmZLTtfBzpEOOnq2y2e'
+  }
+];
+
+for(var i in confs){
+  var item = confs[i];
+  var name = item.name;
+  names.push(name);
+
+  clients[name] = clientCreator(_.assign({}, item, base_conf));
+}
+
+let randomClient = () => {
+  return clients[_.sample(names)]
+};
+
+Object.defineProperty(clients, 'random', {
+  get: randomClient
+});
+
+
+export default clients
--- service/index.js
+++ service/index.js
@@ -1,42 +1,18 @@
 import _ from 'lodash';
 
-import clientCreator from '../util/clientCreator';
+import clients from './client';
 
-let clients = {};
-let names = [];
-let confs = [
-  {
-    name: 'libertyartchen',
-    consumer_key: 'LSOJqOOqefr0zc3nFoTyeIwqs7JeeoHRBE0E6A4N573zwX9V2W',
-    consumer_secret: 'ATdwEZ8DlfprOiUGqReAoHJZAjs5lEHOuDZ8khAvhZzEdYujKM',
-    token: 'LWyVXvxt4AAVT0lMT370pwBRFhoxKna8ibAH2SzSwaqxTNJV9U',
-    token_secret: 'LDgi43Yfd53EezKZyhlU74TkeQRtd5xMmZLTtfBzpEOOnq2y2e'
-  }
-];
-
-for(var i in confs){
-  var item = confs[i];
-  var name = item.name;
-  names.push(name);
-
-  clients[name] = clientCreator(item);
-}
-
-let randomClient = () => {
-  return clients[_.sample(names)]
-};
 
 export default {
   blogInfo: (blogName) => {
-    return randomClient().blogInfo(blogName)
+    return clients.random.blogInfo(blogName)
       .then((data) => {
         return data.blog
       });
   },
   avatar: (param) => {
-    return randomClient().avatar(param.name, param.size)
+    return clients.random.avatar(param.name, param.size)
       .then((data) => {
-        console.log(data);
         return data.avatar_url;
       })
   }
